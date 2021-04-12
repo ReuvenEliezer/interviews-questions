5828e666e0b298bc5e7da207d6bd4a42e47d5e9b
Eliezer <reuvene@mprest.com>
2021-04-12 12:24:36 +0300

    add test

--- src/main/java/MergeOverlappingIntervals.java
+++ src/main/java/MergeOverlappingIntervals.java
@@ -10,36 +10,36 @@ public class MergeOverlappingIntervals {
      */
     @Test
     public void test() {
-        ArrayList<pair> arr = new ArrayList<>();
-        arr.add(new pair(1, 3));
-        arr.add(new pair(2, 4));
-        arr.add(new pair(6, 8));
-        arr.add(new pair(9, 10));
-        ArrayList<pair> pairs = overlappedInterval(arr);
+        ArrayList<Pair> arr = new ArrayList<>();
+        arr.add(new Pair(1, 3));
+        arr.add(new Pair(2, 4));
+        arr.add(new Pair(6, 8));
+        arr.add(new Pair(9, 10));
+        ArrayList<Pair> pairs = overlappedInterval(arr);
     }
 
-    public static ArrayList<pair> overlappedInterval(ArrayList<pair> pairList) {
+    public static ArrayList<Pair> overlappedInterval(ArrayList<Pair> pairList) {
         if (pairList.size() < 2)
             return pairList;
 
-        pairList.sort(Comparator.comparing(pair::getFirst));
-        Collections.sort(pairList, Comparator.comparing(pair::getFirst));
+        pairList.sort(Comparator.comparing(Pair::getFirst));
+        Collections.sort(pairList, Comparator.comparing(Pair::getFirst));
 
         pairList.sort((i1, i2) -> i1.first - i2.first);
-        pairList.sort(new Comparator<pair>() {
+        pairList.sort(new Comparator<Pair>() {
             @Override
-            public int compare(pair i1, pair i2) {
+            public int compare(Pair i1, Pair i2) {
                 return i1.first - i2.first;
             }
         });
-        ArrayList<pair> result = new ArrayList<>();
-        for (pair pair : pairList) {
+        ArrayList<Pair> result = new ArrayList<>();
+        for (Pair pair : pairList) {
             if (result.isEmpty()) {
                 result.add(pair);
             } else {
                 // if current interval is not overlapping with stack top,
                 // push it to the stack
-                pair pair1 = result.get(result.size() - 1);
+                Pair pair1 = result.get(result.size() - 1);
                 if (pair1.second < pair.first)
                     result.add(pair);
 
@@ -53,11 +53,11 @@ public class MergeOverlappingIntervals {
         return result;
     }
 
-    private class pair {
+    private class Pair {
         int first;
         int second;
 
-        public pair(int first, int second) {
+        public Pair(int first, int second) {
             this.first = first;
             this.second = second;
         }
