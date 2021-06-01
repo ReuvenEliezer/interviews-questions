b9ed99a30eb75bc2d0080efa42cf7220ad296fcf
Eliezer <reuvene@mprest.com>
2021-06-01 15:22:31 +0300

    add test

--- src/main/java/TreeInterviews.java
+++ src/main/java/TreeInterviews.java
@@ -8,6 +8,7 @@ public class TreeInterviews {
          * https://practice.geeksforgeeks.org/problems/a512e4b2e812b6df2159b19cc7090ffc1ab056dd/1/?problemType=functional&page=1&query=problemTypefunctionalpage1
          */
 
+
 //        Assert.assertEquals(7, maxSubArrayXOR(new int[]{1, 2, 3, 4}));
     }
 
