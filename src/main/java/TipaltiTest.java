8ad67958634148f19a53f696a28b38e9ecb48747
Eliezer <reuvene@mprest.com>
2021-06-06 13:06:27 +0300

    add test

--- src/main/java/TipaltiTest.java
+++ src/main/java/TipaltiTest.java
@@ -15,10 +15,11 @@ public class TipaltiTest {
         Person joanClarke2 = new Person(new Name("Joan", "Clarke"), new Address("London"));
 
         Person anotherMen = new Person(new Name("Eliezer", "Reuven"), new Address("London"));
-        Person me = new Person(new Name("Eliezer", "Reuven"), new Address("Petah-Tikva"));
-        Person nadavPT = new Person(new Name("Nadav", "Rabinovich"), new Address("Petah-Tikva"));
+        Person mePt = new Person(new Name("Eliezer", "Reuven"), new Address("Petah-Tikva"));
+        Person morPt = new Person(new Name("Moriya", "Reuven"), new Address("Petah-Tikva"));
+        Person nadavCambridge = new Person(new Name("Nadav", "Rabinovich"), new Address("Cambridge"));
 
-        init(new Person[]{graceHopper, alanTuring1, joanClarke1, alanTuring2, joanClarke2, anotherMen, me, nadavPT});
+        init(new Person[]{graceHopper, alanTuring1, joanClarke1, alanTuring2, joanClarke2, anotherMen, mePt, morPt, nadavCambridge});
 
         Assert.assertEquals(-1, findMinRelationLevel(graceHopper, alanTuring1));
         Assert.assertEquals(-1, findMinRelationLevel(alanTuring1, alanTuring1));
@@ -26,9 +27,10 @@ public class TipaltiTest {
         Assert.assertEquals(2, findMinRelationLevel(alanTuring1, joanClarke2));
         Assert.assertEquals(2, findMinRelationLevel(joanClarke2, alanTuring1));
         Assert.assertEquals(3, findMinRelationLevel(anotherMen, alanTuring1));
-        Assert.assertEquals(4, findMinRelationLevel(me, alanTuring1));
-//        Assert.assertEquals(5, findMinRelationLevel(nadavPT, alanTuring1));
-
+        Assert.assertEquals(4, findMinRelationLevel(mePt, alanTuring1));
+        Assert.assertEquals(5, findMinRelationLevel(morPt, alanTuring1));
+        Assert.assertEquals(1, findMinRelationLevel(nadavCambridge, alanTuring2));
+        Assert.assertEquals(2, findMinRelationLevel(nadavCambridge, alanTuring1));
 
         Assert.assertEquals(-1, findMinRelationLevelByQueue(graceHopper, alanTuring1));
         Assert.assertEquals(-1, findMinRelationLevelByQueue(alanTuring1, alanTuring1));
@@ -36,8 +38,10 @@ public class TipaltiTest {
         Assert.assertEquals(2, findMinRelationLevelByQueue(alanTuring1, joanClarke2));
         Assert.assertEquals(2, findMinRelationLevelByQueue(joanClarke2, alanTuring1));
         Assert.assertEquals(3, findMinRelationLevelByQueue(anotherMen, alanTuring1));
-        Assert.assertEquals(4, findMinRelationLevelByQueue(me, alanTuring1));
-        Assert.assertEquals(5, findMinRelationLevelByQueue(nadavPT, alanTuring1));
+        Assert.assertEquals(4, findMinRelationLevelByQueue(mePt, alanTuring1));
+        Assert.assertEquals(5, findMinRelationLevelByQueue(morPt, alanTuring1));
+        Assert.assertEquals(1, findMinRelationLevelByQueue(nadavCambridge, alanTuring2));
+        Assert.assertEquals(2, findMinRelationLevelByQueue(nadavCambridge, alanTuring1));
     }
 
 
@@ -64,20 +68,26 @@ public class TipaltiTest {
         List<Person> relatedPeople = personToOneLevelPersonsMap.get(personA);
         if (relatedPeople.isEmpty())
             return -1;
-        return findMinRelationLevel(relatedPeople, personB, 0);
+
+        Set<Person> visited = new HashSet<>();
+
+        return findMinRelationLevel(relatedPeople, personB, visited, 0);
     }
 
-    private int findMinRelationLevel(List<Person> relatedPeople, Person personB, int level) {
+    private int findMinRelationLevel(List<Person> relatedPeople, Person personB, Set<Person> visited, int level) {
+        if (visited.contains(personB)) return ++level;
+        visited.add(personB);
         Person personFind = relatedPeople.parallelStream()
                 .filter(person -> person.equals(personB))
                 .findAny()
                 .orElse(null);
+
         if (personFind != null) {
             return ++level;
         }
         //do recursive for each neighbors
         for (Person person : relatedPeople) {
-            return findMinRelationLevel(personToOneLevelPersonsMap.get(personB), person, ++level);
+            return findMinRelationLevel(personToOneLevelPersonsMap.get(personB), person, visited, ++level);
         }
         return -1;
     }
@@ -96,51 +106,49 @@ public class TipaltiTest {
         List<Person> relatedPeople;
         Set<Person> visited = new HashSet<>();
         List<List<Person>> personPathList = new ArrayList<>();
-        LinkedList<Person> people = new LinkedList<>();
+        List<Person> people = new ArrayList<>();
         people.add(personA);
         personPathList.add(people);
         while (!queue.isEmpty()) {
-            Person person = queue.poll();
-            System.out.println("search for : " + person.toString());
-            relatedPeople = personToOneLevelPersonsMap.get(person);
+            Person currentPerson = queue.poll();
+            System.out.println("search for : " + currentPerson.toString());
+            relatedPeople = personToOneLevelPersonsMap.get(currentPerson);
             if (relatedPeople.isEmpty())
                 return -1;
             relatedPeople = relatedPeople.stream().filter(p -> !visited.contains(p)).collect(Collectors.toList());
-            System.out.println("relatedPeople size: " + relatedPeople.size() + " " + relatedPeople);
+            System.out.println(String.format("relatedPeople size: %s, %s", relatedPeople.size(), relatedPeople));
             Person personFind = relatedPeople.parallelStream()
                     .filter(relatedPerson -> relatedPerson.equals(personB))
                     .findAny()
                     .orElse(null);
 
             if (personFind != null) {
-                int maxSize = 0;
-                for (List<Person> linkedLists : personPathList) {
-                    System.out.println(linkedLists.toString());
-                    if (linkedLists.size() > maxSize) {
-                        maxSize = linkedLists.size();
-                    }
-                }
-                return maxSize;
+                return personPathList.stream().max(Comparator.comparingInt(List::size)).get().size();
             }
 
-
-            //duplicate path by last person
-            for (int i = 0; i < relatedPeople.size(); i++) {
-                List<Person> list = null;
-                for (List<Person> personPath : personPathList) {
-                    if (personPath.get(personPath.size() - 1).equals(person)) {
-                        list = new ArrayList<>(personPath);
-                        list.add(relatedPeople.get(i));
-                        break;
-                    }
-                }
-                if (list != null)
-                    personPathList.add(list);
-            }
+            duplicatePathByLastPerson(relatedPeople, personPathList, currentPerson);
             queue.addAll(relatedPeople);
-            visited.add(person);
+            visited.add(currentPerson);
         }
         return -1;
+    }
+
+    private void duplicatePathByLastPerson(List<Person> relatedPeople, List<List<Person>> personPathList, Person currentPerson) {
+        List<List> listToAdd = new ArrayList<>();
+        for (Person relatedPerson : relatedPeople) {
+            for (List<Person> personPath : personPathList) {
+                if (personPath.get(personPath.size() - 1).equals(currentPerson)) {
+                    List<Person> listToDuplicate = new ArrayList<>(personPath);
+                    listToDuplicate.add(relatedPerson);
+                    listToAdd.add(listToDuplicate);
+                    System.out.println(String.format("create new path for relatedPerson %s on list: %s ", relatedPerson, listToDuplicate));
+                    break;
+                }
+            }
+        }
+        for (List<Person> list : listToAdd) {
+            personPathList.add(list);
+        }
     }
 
 //    class CustomArrayDeque<E> extends ArrayDeque<E> {
