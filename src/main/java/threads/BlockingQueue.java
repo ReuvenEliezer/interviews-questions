10a86a569f603c94d1e125950df98401c5289d3f
eliezer <eliezer884@gmail.com>
2022-05-14 21:57:21 +0300

    BlockingQueue impl

--- src/main/java/threads/BlockingQueue.java
+++ src/main/java/threads/BlockingQueue.java
@@ -1,19 +1,51 @@
 package threads;
 
+import lombok.SneakyThrows;
+import org.junit.Test;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 public class BlockingQueue<T> {
-    private List<T> list = new ArrayList<>();
+    /**
+     * https://www.geeksforgeeks.org/blockingqueue-interface-in-java/
+     */
+
+    ExecutorService executorService = new ScheduledThreadPoolExecutor(2);
+    private List<T> list = new LinkedList<>();
 
     public synchronized void put(T t) {
+        System.out.println("put value: " + t);
         list.add(t);
-        notifyAll();
+        System.out.println("notifyAll");
+        notifyAll(); // u can to mark if using wait(timeout) with a timeout value
     }
 
     public synchronized T get() throws InterruptedException {
-        while (list.isEmpty()) wait();
-        return list.get(0);
+        System.out.println("getting...");
+        while (list.isEmpty()) {
+            System.out.println("waiting...");
+            wait();
+//            wait(300);
+        }
+        T value = list.remove(0);
+        System.out.println("get value: " + value);
+        return value;
+    }
+
+    @Test
+    public void test() throws InterruptedException, ExecutionException {
+        BlockingQueue<Integer> blockingQueue = new BlockingQueue();
+        Future<?> get = executorService.submit(blockingQueue::get);
+        Thread.sleep(5000);
+        Future<?> put = executorService.submit(() -> blockingQueue.put(1));
+        Object o = get.get();
+        put.get();
     }
 
 }
