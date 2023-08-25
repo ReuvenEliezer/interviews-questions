0ad0efac99c45f472f3027b2b8aa391e56b580a0
eliezer <eliezer884@gmail.com>
2023-08-25 16:34:05 +0300

    impl MyInternalMap keySet & entrySet() func

--- src/main/java/DynamicYieldTest.java
+++ src/main/java/DynamicYieldTest.java
@@ -12,95 +12,136 @@ import static org.assertj.core.api.Assertions.*;
 public class DynamicYieldTest {
 
     @Test
-    public void test() throws InterruptedException {
-        /**
-         *
-         */
+    public void slidingWindowTest() throws InterruptedException {
+        Duration slidingWindowDuration = Duration.ofSeconds(2);
 
-        receive("www.google.com");
-        receive("www.google.com");
-        receive("www.google.com");
-        assertThat(getMostFrequentSorted(2)).size().isEqualTo(1);
-        assertThat(getMostFrequentSorted(1)).size().isEqualTo(1);
-        assertThat(getMostFrequentSorted(1)).containsExactly("www.google.com");
-        receive("www.mc.com");
-        assertThat(getMostFrequentSorted(2)).size().isEqualTo(2);
-        assertThat(getMostFrequentSorted(1)).containsExactly("www.google.com");
-        receive("www.mc.com");
-        receive("www.mc.com");
-        receive("www.mc.com");
-        assertThat(getMostFrequentSorted(1)).containsExactly("www.mc.com");
+        SlidingWindowCache<String> stringSlidingWindowCache = new SlidingWindowCache<>(slidingWindowDuration);
+        stringSlidingWindowCache.receive("www.google.com");
+        stringSlidingWindowCache.receive("www.google.com");
+        stringSlidingWindowCache.receive("www.google.com");
+        assertThat(stringSlidingWindowCache.getMostFrequentSorted(2)).size().isEqualTo(1);
+        assertThat(stringSlidingWindowCache.getMostFrequentSorted(1)).size().isEqualTo(1);
+        assertThat(stringSlidingWindowCache.getMostFrequentSorted(1)).containsExactly("www.google.com");
+        stringSlidingWindowCache.receive("www.mc.com");
+        assertThat(stringSlidingWindowCache.getMostFrequentSorted(2)).size().isEqualTo(2);
+        assertThat(stringSlidingWindowCache.getMostFrequentSorted(1)).containsExactly("www.google.com");
+        stringSlidingWindowCache.receive("www.mc.com");
+        stringSlidingWindowCache.receive("www.mc.com");
+        stringSlidingWindowCache.receive("www.mc.com");
+        assertThat(stringSlidingWindowCache.getMostFrequentSorted(1)).containsExactly("www.mc.com");
 
-        Thread.sleep(PERIOD_TIME_DURATION.plusSeconds(1).toMillis());
-        receive("www.google.com");
-        assertThat(getMostFrequentSorted(1)).containsExactly("www.google.com");
+        Thread.sleep(slidingWindowDuration.plusSeconds(1).toMillis());
+        stringSlidingWindowCache.receive("www.google.com");
+        assertThat(stringSlidingWindowCache.getMostFrequentSorted(1)).containsExactly("www.google.com");
     }
 
-    private final Map<String, Integer> webUrlToCountMap = new HashMap<>();
-    private final Map<Integer, Set<String>> countToWebUrlMap = new TreeMap<>(Collections.reverseOrder()); // in order to return the result of getMostFrequentSorted in o(1)
-    private final Queue<WebUrlData> webUrlDataQueue = new ArrayDeque<>();
-    private static final Duration PERIOD_TIME_DURATION = Duration.ofSeconds(2);
+    @Test
+    public void singleUrlTest() throws InterruptedException {
+        Duration slidingWindowDuration = Duration.ofSeconds(2);
 
+        SlidingWindowCache<String> stringSlidingWindowCache = new SlidingWindowCache<>(slidingWindowDuration);
+        stringSlidingWindowCache.receive("www.google.com");
 
-    public void receive(String url) {
-        WebUrlData webUrlData = new WebUrlData(url, LocalDateTime.now());
-        webUrlDataQueue.add(webUrlData);
+        List<String> mostFrequent = stringSlidingWindowCache.getMostFrequentSorted(1);
+        assertThat(mostFrequent).containsExactly("www.google.com");
+    }
 
-        Integer prevCount = webUrlToCountMap.get(url);
-        Integer mergeResult = webUrlToCountMap.merge(url, 1, Integer::sum);
+    @Test
+    public void multipleUrlsTest() throws InterruptedException {
+        Duration slidingWindowDuration = Duration.ofSeconds(2);
 
-        //add webUrl to the update counter key
-        countToWebUrlMap.computeIfAbsent(mergeResult, key -> new HashSet<>()).add(url);
+        SlidingWindowCache<String> stringSlidingWindowCache = new SlidingWindowCache<>(slidingWindowDuration);
+        stringSlidingWindowCache.receive("www.google.com");
+        stringSlidingWindowCache.receive("www.mc.com");
+        stringSlidingWindowCache.receive("www.google.com");
+        stringSlidingWindowCache.receive("www.mc.com");
 
-        //reduce the webUrl from the prevCount in the countToWebUrlMap
-        if (prevCount != null) {
-            if (countToWebUrlMap.get(prevCount).size() > 1) {
-                //remove the webUrl from the value list
-                countToWebUrlMap.get(prevCount).remove(url);
-            } else {
-                //remove index
-                countToWebUrlMap.remove(prevCount);
-            }
+        List<String> mostFrequent = stringSlidingWindowCache.getMostFrequentSorted(2);
+        assertThat(mostFrequent).containsExactly("www.mc.com", "www.google.com");
+    }
+
+    @Test
+    public void noUrlsTest() throws InterruptedException {
+        Duration slidingWindowDuration = Duration.ofSeconds(2);
+
+        SlidingWindowCache<String> stringSlidingWindowCache = new SlidingWindowCache<>(slidingWindowDuration);
+
+        List<String> mostFrequent = stringSlidingWindowCache.getMostFrequentSorted(1);
+        assertThat(mostFrequent).isEmpty();
+    }
+
+    @Test
+    public void outdatedUrlsTest() throws InterruptedException {
+        Duration slidingWindowDuration = Duration.ofSeconds(2);
+
+        SlidingWindowCache<String> stringSlidingWindowCache = new SlidingWindowCache<>(slidingWindowDuration);
+        stringSlidingWindowCache.receive("www.google.com");
+        Thread.sleep(slidingWindowDuration.plusSeconds(1).toMillis()); // Wait for the sliding window to expire
+
+        List<String> mostFrequent = stringSlidingWindowCache.getMostFrequentSorted(1);
+        assertThat(mostFrequent).isEmpty();
+    }
+
+    static class SlidingWindowCache<K> {
+        private final Duration periodTimeDuration;
+        private final Map<K, Integer> webUrlToCountMap = new HashMap<>();
+        private final Map<Integer, Set<K>> countToWebUrlMap = new TreeMap<>(Collections.reverseOrder()); // in order to return the result of getMostFrequentSorted in o(1)
+        private final Queue<WebUrlData<K>> webUrlDataQueue = new ArrayDeque<>();
+
+        public SlidingWindowCache(Duration periodTimeDuration) {
+            this.periodTimeDuration = periodTimeDuration;
         }
+
+        record WebUrlData<K>(K url, LocalDateTime currentTime) {
+        }
+
+        /**
+         * This method processes incoming URLs
+         *
+         * @param url
+         */
+        public void receive(K url) {
+            WebUrlData<K> webUrlData = new WebUrlData<>(url, LocalDateTime.now());
+            webUrlDataQueue.add(webUrlData);
+
+            Integer prevCount = webUrlToCountMap.get(url);
+            Integer mergeResult = webUrlToCountMap.merge(url, 1, Integer::sum);
+
+            //add webUrl to the update counter key
+            countToWebUrlMap.computeIfAbsent(mergeResult, key -> new HashSet<>()).add(url);
+
+            //reduce the webUrl from the prevCount in the countToWebUrlMap
+            if (prevCount != null) {
+                if (countToWebUrlMap.get(prevCount).size() > 1) {
+                    //remove the webUrl from the value list
+                    countToWebUrlMap.get(prevCount).remove(url);
+                } else {
+                    //remove index
+                    countToWebUrlMap.remove(prevCount);
+                }
+            }
 //        countToWebUrlMap.computeIfAbsent(prevCount, k -> new HashSet<>()).remove(url);
 //        countToWebUrlMap.computeIfAbsent(merge, k -> new HashSet<>()).add(url);
-    }
-
-    public List<String> getMostFrequentSorted(int k) {
-        if (k <= 0) {
-            throw new IllegalArgumentException(String.format("k=%s must be a positive value", k));
-        }
-        //clean old messages
-        LocalDateTime now = LocalDateTime.now();
-        while (!webUrlDataQueue.isEmpty() && webUrlDataQueue.peek().currentTime.plus(PERIOD_TIME_DURATION).isBefore(now)) { //is olden
-            //remove from queue
-            WebUrlData webUrlData = webUrlDataQueue.remove();
-
-
-            //reduce the counter & increase the prev counter
-            Integer totalCount = webUrlToCountMap.get(webUrlData.url);
-            Set<String> urls = countToWebUrlMap.get(totalCount);
-            if (urls.size() <= 1) {
-                countToWebUrlMap.remove(totalCount);
-            } else {
-                urls.remove(webUrlData.url);
-            }
-            countToWebUrlMap.computeIfAbsent(totalCount - 1, key -> new HashSet<>()).add(webUrlData.url); //increase the prev counter
-
-
-            Integer countValue = webUrlToCountMap.merge(webUrlData.url, -1, Integer::sum);
-            if (countValue == 0) {
-                webUrlToCountMap.remove(webUrlData.url);
-            }
-
         }
 
-        // return results by ordering
+        /**
+         * : This method retrieves the k most frequent URLs, sorted in descending order by frequency in a sliding window time
+         */
+        public List<K> getMostFrequentSorted(int urlSize) {
+            if (urlSize <= 0) {
+                throw new IllegalArgumentException(String.format("k=%s must be a positive value", urlSize));
+            }
+            //clean old messages
+            LocalDateTime now = LocalDateTime.now();
+            while (!webUrlDataQueue.isEmpty() && webUrlDataQueue.peek().currentTime.plus(periodTimeDuration).isBefore(now)) { //is olden
+                cleanOldMessages();
+            }
 
-        return countToWebUrlMap.entrySet().stream()
-                .flatMap(entry -> entry.getValue().stream())
-                .limit(k)
-                .collect(Collectors.toCollection(() -> new ArrayList<>(k)));
+            // return results by ordering
+            return countToWebUrlMap.entrySet().stream()
+                    .flatMap(entry -> entry.getValue().stream())
+                    .limit(urlSize)
+                    .collect(Collectors.toCollection(() -> new ArrayList<>(urlSize)));
 //        List<String> result = new ArrayList<>(k);
 //        for (Map.Entry<Integer, Set<String>> entry : countToWebUrlMap.entrySet()) {
 //            for (String webUrl :  entry.getValue()) {
@@ -111,8 +152,30 @@ public class DynamicYieldTest {
 //            }
 //        }
 //        return result;
+        }
+
+        private void cleanOldMessages() {
+            //remove from queue
+            WebUrlData<K> webUrlData = webUrlDataQueue.remove();
+
+            //reduce the counter & increase the prev counter
+            Integer totalCount = webUrlToCountMap.get(webUrlData.url);
+            Set<K> urls = countToWebUrlMap.get(totalCount);
+            if (urls.size() <= 1) {
+                countToWebUrlMap.remove(totalCount);
+            } else {
+                urls.remove(webUrlData.url);
+            }
+            if (totalCount - 1 > 0) { //don't add value to a zero counter
+                countToWebUrlMap.computeIfAbsent(totalCount - 1, key -> new HashSet<>()).add(webUrlData.url); //increase the prev counter
+            }
+
+            //reduce counter of webUrlToCountMap
+            Integer countValue = webUrlToCountMap.merge(webUrlData.url, -1, Integer::sum);
+            if (countValue == 0) {
+                webUrlToCountMap.remove(webUrlData.url);
+            }
+        }
     }
 
-    record WebUrlData(String url, LocalDateTime currentTime) {
-    }
 }
